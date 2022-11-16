7effc8a7ca03d6c956e1699bce60026a12253ddb
sample-projects <118372925+sample-projects@users.noreply.github.com>
2022-11-17 00:10:06 +0800

    update script.js

--- script.js
+++ script.js
@@ -6,106 +6,100 @@ const dashboardBtn = document.querySelector(".class-button");
 const settingsBtn = document.querySelector(".settings-button");
 
 const getCurrentTime = () => {
-    const date = new Date()
+   const date = new Date();
 
-    var day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+   var day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-    var hour = date.getHours() - 12;
-    var minute = date.getMinutes();
-    minute = minute < 10 ? '0'+minute : minute;
-    var AMPM = date.getHours() > 12 ? "PM" : "AM";
-    hour = hour > 12 ? 12-hour : hour;
-    var time  = `${hour}:${minute}${AMPM}`;
-    var day = day[date.getDay()]
+   var hour = date.getHours() - 12;
+   var minute = date.getMinutes();
+   minute = minute < 10 ? "0" + minute : minute;
+   var AMPM = date.getHours() > 12 ? "PM" : "AM";
+   //hour = hour > 12 ? 12-hour : hour;
+   var time = `${hour}:${minute}${AMPM}`;
+   var day = day[date.getDay()];
 
-    document.getElementById("time").innerText = time;
-    document.getElementById("day").innerText = day;
-    console.log(time)
-    setTimeout(getCurrentTime, 500);
-}
+   document.getElementById("time").innerText = time;
+   document.getElementById("day").innerText = day;
+   console.log(time);
+   setTimeout(getCurrentTime, 500);
+};
 const getCurrentDate = () => {
-    if(document.getElementById("date_") != null){
-        const date = new Date()
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
+   if (document.getElementById("date_") != null) {
+      const date = new Date();
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
 
-        var currentDate = `${year}-${month}-${day}`;
+      var currentDate = `${year}-${month}-${day}`;
 
-        // console.log(currentDate);
-        document.getElementById("date_").value = currentDate;
-    }
-}
+      // console.log(currentDate);
+      document.getElementById("date_").value = currentDate;
+   }
+};
 
 getCurrentTime();
 getCurrentDate();
 
-studentBtn.addEventListener('click', function () {
-    if(window.location.href != window.location.origin + "/student.html"){
-        window.location.href = "student.html";
-    }
-})
+studentBtn.addEventListener("click", function () {
+   if (window.location.href != window.location.origin + "/student.html") {
+      window.location.href = "student.html";
+   }
+});
 
-dashboardBtn.addEventListener('click', function () {
-    if(window.location.href != window.location.origin + "/index.html"){
-        window.location.href = "index.html";
-    }
-})
-settingsBtn.addEventListener('click', function () {
-    if(window.location.href != window.location.origin + "/settings.html"){
-        window.location.href = "settings.html";
-    }
-})
+dashboardBtn.addEventListener("click", function () {
+   if (window.location.href != window.location.origin + "/index.html") {
+      window.location.href = "index.html";
+   }
+});
+settingsBtn.addEventListener("click", function () {
+   if (window.location.href != window.location.origin + "/settings.html") {
+      window.location.href = "settings.html";
+   }
+});
 
-var comment = document.createComment("Jones lapinig")
-document.body.appendChild(comment)
+var comment = document.createComment("Jones lapinig");
+document.body.appendChild(comment);
 
-
-
-if(window.location.href == window.location.origin + "/student.html"){
-    console.log("Hello")
-    const filterContainer = document.querySelector(".filter-container");
-    const sortContainer = document.querySelector(".sort-list");
-    const filterBtn = document.querySelector(".filter");
-    const sortBtn = document.querySelector(".sort");
-    filterBtn.addEventListener('click', function () {
-        filterContainer.style.display = "flex";
-        return;
-    })
-    sortBtn.addEventListener('click', function () {
-        sortContainer.style.display = "flex";
-        return;
-    })
+if (window.location.href == window.location.origin + "/student.html") {
+   console.log("Hello");
+   const filterContainer = document.querySelector(".filter-container");
+   const sortContainer = document.querySelector(".sort-list");
+   const filterBtn = document.querySelector(".filter");
+   const sortBtn = document.querySelector(".sort");
+   filterBtn.addEventListener("click", function () {
+      filterContainer.style.display = "flex";
+      return;
+   });
+   sortBtn.addEventListener("click", function () {
+      sortContainer.style.display = "flex";
+      return;
+   });
 }
 
-document.addEventListener('click', (event) => {
-    if(event.target.tagName == "svg") {
-        return;
-    }
-    else if(!event.target.closest(".menu")){
-        menu.style.display = "none";
-    }
+document.addEventListener("click", (event) => {
+   if (event.target.tagName == "svg") {
+      return;
+   } else if (!event.target.closest(".menu")) {
+      menu.style.display = "none";
+   }
 
-    if(window.location.hre == window.location.origin + "/student.html"){
-        if(event.target == "circle") {
-            filterContainer.style.display = "none";
-        }
-        else if(!event.target.closest(".filter-container")){
-            filterContainer.style.display = "none";
-        }
-        if(event.target == "div.sort") {
-            sortContainer.style.display = "none";
-        }
-        else if(!event.target.closest(".sort-list")){
-            sortContainer.style.display = "none";
-        }
-    }
-})
+   if (window.location.hre == window.location.origin + "/student.html") {
+      if (event.target == "circle") {
+         filterContainer.style.display = "none";
+      } else if (!event.target.closest(".filter-container")) {
+         filterContainer.style.display = "none";
+      }
+      if (event.target == "div.sort") {
+         sortContainer.style.display = "none";
+      } else if (!event.target.closest(".sort-list")) {
+         sortContainer.style.display = "none";
+      }
+   }
+});
 
+menuBtn.addEventListener("click", function () {
+   menu.style.display = "flex";
+   return;
+});
 
-menuBtn.addEventListener('click', function () {
-    menu.style.display = "flex";
-    return;
-})
-
-console.log(window.location.href)
+console.log(window.location.href);

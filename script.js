c43b4c9649fd5bcae407d6edfd747741f6b82bc2
sample-projects <118372925+sample-projects@users.noreply.github.com>
2022-11-17 00:29:16 +0800

    update script.js

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ const getCurrentTime = () => {
 
    var day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-   var hour = date.getHours() - 12;
+   var hour = date.getHours();
    var minute = date.getMinutes();
    minute = minute < 10 ? "0" + minute : minute;
    var AMPM = date.getHours() > 12 ? "PM" : "AM";
-   hour = hour > 12 ? hour-12 : hour;
+   hour = hour > 12 ? hour - 12 : hour;
+   hour = hour == 0 ? 12 : hour;
    var time = `${hour}:${minute}${AMPM}`;
    var day = day[date.getDay()];
 

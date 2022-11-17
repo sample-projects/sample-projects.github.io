17ca47d8368ff72a19839cdf5e8b82e0f7b3000c
jonestly-source <45253980+jonestly-source@users.noreply.github.com>
2022-11-17 16:10:00 +0800

    register-backend works

--- registration.php
+++ registration.php
@@ -2,6 +2,11 @@
 
     require 'connection.php';
     $display = "none";
+    $date = date("Y-m-d");
+    $studentId = '';
+    $studentName = '';
+    $gender = '';
+    $section = '';
 
 
     if (isset($_POST['submit'])) {
@@ -9,15 +14,13 @@
         $studentName = $_POST['name'];
         $section = $_POST['section'];
         $gender = $_POST['gender'];
-        $display = "none";
 
         //Inserting into Database
 
-        $sql = "INSERT INTO students VALUES ('$studentId', '$studentName', '$gender', 'null', '$section')";
+        $sql = "INSERT INTO students VALUES ('$studentId', '$studentName', '$gender', 'null', '$section', '$date')";
 
         if (mysqli_query($connection, $sql)) {
             $display = "flex";
-            echo '<script src="qrCode.js"></script>';
         }
         else {
             $display = "none";
@@ -32,24 +35,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <!-- <script src="qrCode.js" defer></script> -->
     <title>Register</title>
 </head>
 <body>
     <div class="container">
         <h1>Register</h1>
-        <form class="content" method="POST" action="">
+        <form class="content" method="POST" action="#">
             <div class="studentId input-group">
                 <label for="studentId">Student ID</label>
-                <input type="text" name="studentId" id="studentId" required>
+                <input type="text" name="studentId" id="studentId" required value="<?php echo $studentId ?>">
             </div>
             <div class="name input-group">
                 <label for="name">Name</label>
-                <input type="text" name="name" id="name" required>
+                <input type="text" name="name" id="name" required value="<?php echo $studentName ?>">
             </div>
             <div class="gender input-group">
                 <label for="gender">Gender</label>
-                <input type="radio" name="gender" id="gender" value="male" checked required>
+                <input type="radio" name="gender" id="gender" value="male" value="<?php echo $gender ?>" required>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-gender-male" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="var(--male-color)" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <circle cx="10" cy="14" r="5"></circle>
@@ -67,7 +69,7 @@
             </div>
             <div class="section input-group">
                 <label for="section">Section</label>
-                <input type="text" name="section" id="section" required>
+                <input type="text" name="section" id="section" value="<?php echo $section ?>" required>
             </div>
             <input type="submit" id="submitBtn" name="submit" value="Register">
             <div class="qrContainer" style="display: <?= $display ?>">
@@ -76,7 +78,7 @@
             </div>
         </form>
     </div>
-    <!-- <script src="script.js"></script> -->
+    <script src="qrCode.js"></script>
 </body>
 </html>
 

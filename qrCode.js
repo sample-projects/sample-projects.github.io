e9308a61979bb6796d171cb7dcf6be20d4f7c666
jonestly-source <45253980+jonestly-source@users.noreply.github.com>
2022-11-17 16:10:51 +0800

    qrcode generator works

--- qrCode.js
+++ qrCode.js
@@ -1,9 +1,7 @@
 const qrImg = document.getElementById("qrImg");
 const studentId = document.getElementById("studentId");
-console.log("HELLO")
 
-let qrValue = studentId.value;
-
-if(!qrValue) return;
-
-qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+if (studentId.value != '') {
+    let qrValue = studentId.value; 
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+}
